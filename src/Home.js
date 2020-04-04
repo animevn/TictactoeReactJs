c9b6fcd244f987745e10d4ccf4ebababf56c527f
animevn <haanhth21@gmail.com>
2020-04-04 22:11:13 +0700

    layout ok now

--- src/Home.js
+++ src/Home.js
@@ -4,8 +4,19 @@ import "./Home.css";
 function Home() {
 
   return (
-    <div className="container mt-3">
-      <h2 className="text-center">X to play</h2>
+    <div className="container">
+
+      <div className="container col-11 col-sm-11 col-md-9 col-lg-7 col-xl-6">
+        <button className="btn btn-lg btn-danger rounded-circle button-add shadow-lg">
+          +
+        </button>
+      </div>
+
+      <div className="container col-8 col-sm-8 col-md-6 col-lg-4 col-xl-4">
+        <h2 className="text-center">X to play</h2>
+      </div>
+
+
       <div className="container mt-3">
         <div className="row d-flex flex-row justify-content-center">
           <button className="btn rounded-0 border-left border-top"
@@ -60,10 +71,10 @@ function Home() {
         <img className="first-image btn btn-outline-success "
              src="/images/first.svg" alt="first"/>
 
-        <img className="next-image btn btn-outline-success "
+        <img className="first-image btn btn-outline-success "
              src="/images/back.svg" alt="back"/>
 
-        <img className="next-image btn btn-outline-success "
+        <img className="first-image btn btn-outline-success "
              src="/images/next.svg" alt="next"/>
 
         <img className="first-image btn btn-outline-success "
