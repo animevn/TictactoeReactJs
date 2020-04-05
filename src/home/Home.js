b9478ff4a5028243e9dad055c0737c2f28c20a99
animevn <haanhth21@gmail.com>
2020-04-05 10:54:18 +0700

    separate view

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,17 +1,16 @@
 import React, {useContext} from "react";
 import "./Home.css";
-import {BoardContext, clearCells, makeMove, inform} from "./HomeModel";
+import {BoardContext, clearCells, makeMove} from "./HomeModel";
+import HomeView from "./HomeView";
 
 function Home() {
   const {myboard, setMyboard} = useContext(BoardContext);
 
-  function reset(event) {
-    event.preventDefault();
+  function reset() {
     setMyboard(old=>clearCells(old));
   }
 
   function onClick(event) {
-    event.preventDefault();
     const id = event.target.id;
     const row = id.substring(0, 1);
     const col = id.substring(1, 2);
@@ -19,93 +18,7 @@ function Home() {
   }
 
   return (
-    <div className="container">
-
-      <div className="container col-11 col-sm-11 col-md-9 col-lg-7 col-xl-6">
-        <button className="btn btn-lg btn-danger rounded-circle button-add shadow-lg"
-                onClick={reset}>
-          +
-        </button>
-      </div>
-
-      <div className="container col-8 col-sm-8 col-md-6 col-lg-4 col-xl-4 mt-3">
-        <h2 className="text-center">{inform(myboard)}</h2>
-      </div>
-
-      <div className="container mt-3">
-        <div className="row d-flex flex-row justify-content-center">
-          <button className="btn rounded-0 border-left border-top"
-                  style={{"width":"5rem", "height":"5rem"}}
-                  id="00" onClick={onClick}>
-            {myboard.gameboard[0][0]}
-          </button>
-          <button className="btn rounded-0 border-left border-top"
-                  style={{"width":"5rem", "height":"5rem"}}
-                  id="01" onClick={onClick}>
-            {myboard.gameboard[0][1]}
-          </button>
-          <button className="btn rounded-0 border-left border-top border-right"
-                  style={{"width":"5rem", "height":"5rem"}}
-                  id="02" onClick={onClick}>
-            {myboard.gameboard[0][2]}
-          </button>
-        </div>
-
-        <div className="row d-flex flex-row justify-content-center">
-          <button className="btn rounded-0 border-left border-top"
-                  style={{"width":"5rem", "height":"5rem"}}
-                  id="10" onClick={onClick}>
-            {myboard.gameboard[1][0]}
-          </button>
-          <button className="btn rounded-0 border-left border-top"
-                  style={{"width":"5rem", "height":"5rem"}}
-                  id="11" onClick={onClick}>
-            {myboard.gameboard[1][1]}
-          </button>
-          <button className="btn rounded-0 border-left border-top border-right"
-                  style={{"width":"5rem", "height":"5rem"}}
-                  id="12" onClick={onClick}>
-            {myboard.gameboard[1][2]}
-          </button>
-        </div>
-
-        <div className="row d-flex flex-row justify-content-center">
-          <button className="btn rounded-0 border-left border-top border-bottom"
-                  style={{"width":"5rem", "height":"5rem"}}
-                  id="20" onClick={onClick}>
-            {myboard.gameboard[2][0]}
-          </button>
-          <button className="btn rounded-0 border-left border-top border-bottom"
-                  style={{"width":"5rem", "height":"5rem"}}
-                  id="21" onClick={onClick}>
-            {myboard.gameboard[2][1]}
-          </button>
-          <button className="btn rounded-0 border-left border-top border-right border-bottom"
-                  style={{"width":"5rem", "height":"5rem"}}
-                  id="22" onClick={onClick}>
-            {myboard.gameboard[2][2]}
-          </button>
-        </div>
-
-      </div>
-
-      <div className="container col-11 col-sm-11 col-md-9 col-lg-7 col-xl-6 mx-auto mt-5
-                      d-flex flex-row justify-content-around">
-
-        <img className="first-image btn btn-outline-success "
-             src="/images/first.svg" alt="first"/>
-
-        <img className="first-image btn btn-outline-success "
-             src="/images/back.svg" alt="back"/>
-
-        <img className="first-image btn btn-outline-success "
-             src="/images/next.svg" alt="next"/>
-
-        <img className="first-image btn btn-outline-success "
-             src="/images/last.svg" alt="last"/>
-      </div>
-
-    </div>
+    <HomeView myboard={myboard} reset={reset} onClick={onClick}/>
   )
 }
 
---
+++ src/home/HomeView.js
@@ -0,0 +1,109 @@
+import React from "react";
+import "./Home.css";
+import {inform} from "./HomeModel";
+
+function HomeView(props) {
+  const myboard = props.myboard;
+
+  function reset(event) {
+    event.preventDefault();
+    props.reset()
+  }
+
+  function onClick(event) {
+    event.preventDefault();
+    props.onClick(event);
+  }
+
+  return (
+    <div className="container">
+
+      <div className="container col-11 col-sm-11 col-md-9 col-lg-7 col-xl-6">
+        <button className="btn btn-lg btn-danger rounded-circle button-add shadow-lg"
+                onClick={reset}>
+          +
+        </button>
+      </div>
+
+      <div className="container col-8 col-sm-8 col-md-6 col-lg-4 col-xl-4 mt-3">
+        <h2 className="text-center">{inform(myboard)}</h2>
+      </div>
+
+      <div className="container mt-3">
+        <div className="row d-flex flex-row justify-content-center">
+          <button className="btn rounded-0 border-left border-top"
+                  style={{"width":"5rem", "height":"5rem"}}
+                  id="00" onClick={onClick}>
+            {myboard.gameboard[0][0]}
+          </button>
+          <button className="btn rounded-0 border-left border-top"
+                  style={{"width":"5rem", "height":"5rem"}}
+                  id="01" onClick={onClick}>
+            {myboard.gameboard[0][1]}
+          </button>
+          <button className="btn rounded-0 border-left border-top border-right"
+                  style={{"width":"5rem", "height":"5rem"}}
+                  id="02" onClick={onClick}>
+            {myboard.gameboard[0][2]}
+          </button>
+        </div>
+
+        <div className="row d-flex flex-row justify-content-center">
+          <button className="btn rounded-0 border-left border-top"
+                  style={{"width":"5rem", "height":"5rem"}}
+                  id="10" onClick={onClick}>
+            {myboard.gameboard[1][0]}
+          </button>
+          <button className="btn rounded-0 border-left border-top"
+                  style={{"width":"5rem", "height":"5rem"}}
+                  id="11" onClick={onClick}>
+            {myboard.gameboard[1][1]}
+          </button>
+          <button className="btn rounded-0 border-left border-top border-right"
+                  style={{"width":"5rem", "height":"5rem"}}
+                  id="12" onClick={onClick}>
+            {myboard.gameboard[1][2]}
+          </button>
+        </div>
+
+        <div className="row d-flex flex-row justify-content-center">
+          <button className="btn rounded-0 border-left border-top border-bottom"
+                  style={{"width":"5rem", "height":"5rem"}}
+                  id="20" onClick={onClick}>
+            {myboard.gameboard[2][0]}
+          </button>
+          <button className="btn rounded-0 border-left border-top border-bottom"
+                  style={{"width":"5rem", "height":"5rem"}}
+                  id="21" onClick={onClick}>
+            {myboard.gameboard[2][1]}
+          </button>
+          <button className="btn rounded-0 border-left border-top border-right border-bottom"
+                  style={{"width":"5rem", "height":"5rem"}}
+                  id="22" onClick={onClick}>
+            {myboard.gameboard[2][2]}
+          </button>
+        </div>
+
+      </div>
+
+      <div className="container col-11 col-sm-11 col-md-9 col-lg-7 col-xl-6 mx-auto mt-5
+                      d-flex flex-row justify-content-around">
+
+        <img className="first-image btn btn-outline-success "
+             src="/images/first.svg" alt="first"/>
+
+        <img className="first-image btn btn-outline-success "
+             src="/images/back.svg" alt="back"/>
+
+        <img className="first-image btn btn-outline-success "
+             src="/images/next.svg" alt="next"/>
+
+        <img className="first-image btn btn-outline-success "
+             src="/images/last.svg" alt="last"/>
+      </div>
+
+    </div>
+  )
+}
+
+export default HomeView;
